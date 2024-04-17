a1faee51bcb7aa9e0c04a4b5e489c740f4fdc6ed
Tea <tego3046@colorado.edu>
2024-04-16 22:01:32 -0600

    wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,40 +1,40 @@
-const btn = document.querySelector("#js-new-quote");
-btn.addEventListener('click', getQuote);
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector("#js-new-quote");
+    btn.addEventListener('click', getQuote);
 
-const answerBtn = document.querySelector("#js-tweet"); 
-answerBtn.addEventListener('click', getAnswer); 
+    const answerBtn = document.querySelector("#js-tweet");
+    answerBtn.addEventListener('click', getAnswer);
 
-const answerText = document.querySelector('#js-answer-text');
+    const answerText = document.querySelector('#js-answer-text');
 
-const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+    const endpoint = "https://api.quotable.io/random";
 
-let answer = '';
+    let answer = '';
 
-async function getQuote() {
-    try {
-        const response = await fetch(endpoint);
-        if (!response.ok) {
-            throw Error(response.statusText)
+    async function getQuote() {
+        try {
+            const response = await fetch(endpoint);
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            const json = await response.json();
+            console.log(json);
+            displayQuote(json.content);
+            answer = json.author;
+        } catch (err) {
+            console.error("Error fetching quote:", err.message);
+            alert('Failed to fetch new quote');
         }
-        const json = await response.json();
-        console.log(json["question"]);
-        displayQuote(json['question']);
-        console.log(json['answer']);
-        answer = json['answer'];
-    } catch (err) {
-        console.log(err);
-        alert('Failed to fetch new quote');
     }
-}
 
-function displayQuote(quote) {
-    const quotetext = document.querySelector("#js-quote-text");
-    quotetext.textContent = quote;
-}
+    function displayQuote(quote) {
+        const quotetext = document.querySelector("#js-quote-text");
+        quotetext.textContent = quote;
+    }
 
-function getAnswer() {
-    answerText.textContent = answer;
-}
+    function getAnswer() {
+        answerText.textContent = answer;
+    }
 
-getQuote()
-
+    getQuote();
+});
